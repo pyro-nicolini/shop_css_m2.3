0471f5c5bd541d93478e95273bd74a4413f59e83
Piero Nicolini <naturalnen@gmail.com>
2024-11-15 15:46:58 -0300

    fix layout

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const parallaxBg = document.querySelector('.parallax-bg');
-    const scrollY = window.scrollY;
-    parallaxBg.style.transform = `translateY(${scrollY * 0.4}px)`;
-  });
-
-const video = document.querySelector('.video-background');
-video.playbackRate = 0.8;
-   
